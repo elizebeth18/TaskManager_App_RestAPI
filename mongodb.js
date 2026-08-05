7a804efdb3a627474d4fc6a255a6fd3ae322f471
Jilu Elizebeth <elizebeth18.jilu@gmail.com>
2026-08-05 13:47:56 +0530

    use findOne

--- mongodb.js
+++ mongodb.js
@@ -1,8 +1,8 @@
 //CRUD - Create Read Update Delete
 
-const mongodb = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 
-const MongoClient = mongodb.MongoClient;
+//const MongoClient = mongodb.MongoClient;
 const connectionURL = 'mongodb://127.0.0.1:27017';
 
 const client = new MongoClient(connectionURL);
@@ -14,36 +14,45 @@ const startDBServer = async () => {
 
     const db = client.db(databaseName); //creates a new DB using databaseName
     const users = db.collection('users'); //creates a collection
-    const result = await users.insertOne({
-        name: "Jisha",
-        age: 32
-    });
+    // const result = await users.insertOne({
+    //     name: "Jisha",
+    //     age: 32
+    // });
 
-    const result = await users.insertMany([{
-        name: "Job",
-        age: 22,
-    },
-    {
-        name: "Jibu",
-        age: 26,
-    }]);
+    // const result = await users.insertMany([{
+    //     name: "Job",
+    //     age: 22,
+    // },
+    // {
+    //     name: "Jibu",
+    //     age: 26,
+    // }]);
 
-    const tasks = db.collection("tasks");
-    const result = await tasks.insertMany([
-        {
-            description: "Buy groceries",
-            completed: true,
-        },
-        {
-            description: "Mopping house",
-            completed: false,
-        },
-        {
-            description: "Cooking dinner",
-            completed: true
-        }
-    ]);
-    console.log(result);
+    // const tasks = db.collection("tasks");
+    // const result = await tasks.insertMany([
+    //     {
+    //         description: "Buy groceries",
+    //         completed: true,
+    //     },
+    //     {
+    //         description: "Mopping house",
+    //         completed: false,
+    //     },
+    //     {
+    //         description: "Cooking dinner",
+    //         completed: true
+    //     }
+    // ]);
+
+    const user = await db.collection('users').findOne({
+        _id: new ObjectId("6a72020c430a9d3aedfde87d")
+    })
+
+    const user1 = await db.collection("users").findOne({name: "Ebin"});
+    
+    console.log(user);
+
+    console.log(user1);
 
     //await client.close();
 }
