f09f8f15ec00a6036e8257a47d8f4fa03fc0393b
Jilu Elizebeth <elizebeth18.jilu@gmail.com>
2026-08-05 12:56:09 +0530

    create collections, use insertOne and insertMany

--- mongodb.js
+++ mongodb.js
@@ -6,10 +6,46 @@ const MongoClient = mongodb.MongoClient;
 const connectionURL = 'mongodb://127.0.0.1:27017';
 
 const client = new MongoClient(connectionURL);
+const databaseName = "task-manager";
 
-const startServer = async () => {
+const startDBServer = async () => {
     await client.connect();
-    console.log("DB Connected!!")
+    console.log("MongoDB Connected!!")
+
+    const db = client.db(databaseName); //creates a new DB using databaseName
+    const users = db.collection('users'); //creates a collection
+    const result = await users.insertOne({
+        name: "Jisha",
+        age: 32
+    });
+
+    const result = await users.insertMany([{
+        name: "Job",
+        age: 22,
+    },
+    {
+        name: "Jibu",
+        age: 26,
+    }]);
+
+    const tasks = db.collection("tasks");
+    const result = await tasks.insertMany([
+        {
+            description: "Buy groceries",
+            completed: true,
+        },
+        {
+            description: "Mopping house",
+            completed: false,
+        },
+        {
+            description: "Cooking dinner",
+            completed: true
+        }
+    ]);
+    console.log(result);
+
+    //await client.close();
 }
 
-startServer();
+startDBServer();
