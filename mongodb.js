41a694e08e90578d23ed0ae2fb434b6a20732ab3
Jilu Elizebeth <elizebeth18.jilu@gmail.com>
2026-08-05 18:30:10 +0530

    all CRUD operations

--- mongodb.js
+++ mongodb.js
@@ -14,19 +14,19 @@ const startDBServer = async () => {
 
     const db = client.db(databaseName); //creates a new DB using databaseName
     const users = db.collection('users'); //creates a collection
-    // const result = await users.insertOne({
-    //     name: "Jisha",
-    //     age: 32
-    // });
+    const result = await users.insertOne({
+        name: "Jisha",
+        age: 32
+    });
 
-    // const result = await users.insertMany([{
-    //     name: "Job",
-    //     age: 22,
-    // },
-    // {
-    //     name: "Jibu",
-    //     age: 26,
-    // }]);
+    const resultMany = await users.insertMany([{
+        name: "Job",
+        age: 22,
+    },
+    {
+        name: "Jibu",
+        age: 26,
+    }]);
 
     // const tasks = db.collection("tasks");
     // const result = await tasks.insertMany([
@@ -48,13 +48,44 @@ const startDBServer = async () => {
         _id: new ObjectId("6a72020c430a9d3aedfde87d")
     })
 
-    const user1 = await db.collection("users").findOne({name: "Ebin"});
+    const user1 = await db.collection("users").find({ name: "Ebin" }).limit(2).toArray();
+
+    //console.log(user);
+
+    //console.log(user1);
+
+    const lastTask = await db.collection("tasks").findOne({
+        _id: new ObjectId("6a721a6733cb6d468808881b")
+    });
+
+    console.log(lastTask);
+
+    const inCompletedTasks = await db.collection("tasks").find({ completed: false }).limit(5).toArray();
+
+    //console.log("Incompleted Tasks :", inCompletedTasks);
+
+    // const result = await db.collection("users").updateOne({ _id: new ObjectId("6a72020c430a9d3aedfde87d") }, {
+    //     $set: {
+    //         name: "Shwetha"
+    //     }
+    // });
+
+    const setTaskCompleted = await db.collection("tasks")
+        .updateMany({ completed: false }, {
+            $set: {
+                completed: true
+            }
+        });
+
+    const deleteUsers = await db.collection("users").deleteMany({
+        name: 'Jibu'
+    })
     
-    console.log(user);
+    const deleteUser = await db.collection("users").deleteOne({
+        _id: new ObjectId("6a7330de48a0a8abca772ba6")
+    })
 
-    console.log(user1);
-
-    //await client.close();
+    console.log(deleteUsers)
 }
 
 startDBServer();
