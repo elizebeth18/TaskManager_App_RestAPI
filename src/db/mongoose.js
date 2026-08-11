224cf7893b420c28c63891d72f84c0a3b9b84dd0
Jilu Elizebeth <elizebeth18.jilu@gmail.com>
2026-08-11 13:41:22 +0530

    create new task using app.post and store it in mongoDB

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 const connectDB = async () => {
     await mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api");
@@ -7,52 +6,3 @@ const connectDB = async () => {
 }
 
 connectDB();
-
-
-
-const taskSchema = new mongoose.Schema({
-    task: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    completed: {
-        type: String,
-        default: false,
-    }
-});
-const Task = mongoose.model("Task", taskSchema);
-
-
-const learnMongooseTask = new Task({
-    task: "Drawing",
-});
-
-const savedTask = async () => {
-    try {
-        const data = await learnMongooseTask.save();
-        console.log("Success! Saved document:", data);
-    } catch (error) {
-        console.error("Database save failed:", error.message);
-    }
-}
-
-//savedTask();
-
-// const user = new User({
-//     name: "        Jenny     ",
-//     email: " MEMAIL@ME.COM      ",
-//     age: 45,
-//     password: "Jenn@123"
-// });
-
-const savedUser = async () => {
-    try {
-        const data = await user.save();
-        console.log("Success! Saved document:", data);
-    } catch (error) {
-        console.error("Database save failed:", error.message);
-    }
-}
-
-//savedUser();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("./db/mongoose");
 const User = require("./models/user");
+const Task = require('./models/tasks');
 
 const app = express();
 
@@ -9,16 +10,26 @@ app.use(express.json());
 app.post("/users", (req, res, next) => {
 
     const user = new User(req.body);
-    
+
     // Explicitly wait for the database to complete the save operation
     user.save().then((savedUser) => {
         // Respond with an appropriate HTTP status code
         res.status(201).send(savedUser);
     })
-    .catch((error) => {
+        .catch((error) => {
             res.status(400).send(error)
         });
+});
 
+app.post("/tasks", (req, res, next) => {
+
+    const task = new Task(req.body);
+
+    task.save().then((savedTask) => {
+        res.status(201).send(savedTask)
+    }).catch((err)=>{
+        res.status(400).send(err)
+    })
 });
 
 app.listen(3000, () => {
