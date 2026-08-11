d14fa8464e805e1d940f522ae8ede7139c0cfc26
Jilu Elizebeth <elizebeth18.jilu@gmail.com>
2026-08-11 16:10:30 +0530

    api to fetch users list  and  task list and specific users and task

--- src/index.js
+++ src/index.js
@@ -21,14 +21,64 @@ app.post("/users", (req, res, next) => {
         });
 });
 
+app.get("/users", (req, res, next) => {
+    User.find({}).then((users) => {
+        res.status(200).send(users);
+    }).catch((error) => {
+        res.status(500).send();
+    })
+});
+
+app.get("/users/:id", (req, res, next) => {
+
+    const _id = req.params.id;
+
+    User.findById(_id).exec().then((result) => {
+
+        if (!result) {
+            return res.status(404).send({ message: "User not found" })
+        }
+
+        res.status(200).send(result);
+
+    }).catch((error) => {
+        res.status(500).send(error);
+    });
+})
+
 app.post("/tasks", (req, res, next) => {
 
     const task = new Task(req.body);
 
     task.save().then((savedTask) => {
         res.status(201).send(savedTask)
-    }).catch((err)=>{
+    }).catch((err) => {
         res.status(400).send(err)
+    })
+});
+
+app.get("/tasks", (req, res, next) => {
+
+    Task.find({}).then((result) => {
+        res.status(200).send(result)
+    }).catch((error) => {
+        res.status(500).send(error);
+    });
+});
+
+app.get("/tasks/:id", (req, res, next) => {
+
+    const _id = req.params.id;
+
+    Task.findById(_id).exec().then((result) => {
+
+        if(!result){
+            return res.status(404).send({ message: "Task not found" })
+        }
+
+        res.status(200).send(result);
+    }).catch((error) => {
+        res.send(error)
     })
 });
 
