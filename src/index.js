b4ba520e3bbab0533da3511909c5aa1efb289004
Jilu Elizebeth <elizebeth18.jilu@gmail.com>
2026-08-12 16:19:46 +0530

    understand patch and delete http request

--- src/index.js
+++ src/index.js
@@ -51,6 +51,50 @@ app.get("/users/:id", async (req, res, next) => {
 
 });
 
+app.patch("/users/:id", async (req, res, next) => {
+
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['name', 'email', 'password', 'age'];
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: "Invalid updates" });
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {
+                new: true,
+                runValidators: true
+            }
+        );
+
+        if (!user) {
+            return res.status(404).send();
+        }
+
+        res.send(user);
+
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+app.delete("/users/:id", async (req, res, next) => {
+    try {
+        const deletedUser = await User.findByIdAndDelete(req.params.id);
+        if (!deletedUser) {
+            return res.status(404).send({ error: "User not found" })
+        }
+
+        res.send(deletedUser);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
 app.post("/tasks", async (req, res, next) => {
 
     try {
@@ -87,6 +131,49 @@ app.get("/tasks/:id", async (req, res, next) => {
 
     } catch (error) {
         res.status(500).send(error)
+    }
+});
+
+app.patch("/tasks/:id", async (req, res, next) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['task', 'completed'];
+
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: "Invalid updates" });
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {
+                new: true,
+                runValidators: true
+            }
+        );
+
+        if (!task) {
+            return res.status(404).send();
+        }
+
+        res.send(task);
+
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+app.delete("/tasks/:id", async (req, res, next) => {
+    try {
+        const deletedTask = await Task.findByIdAndDelete(req.params.id);
+        if (!deletedTask) {
+            return res.status(404).send({ error: "Task not found" })
+        }
+        res.send(deletedTask);
+    } catch (error) {
+        res.status(500).send(error);
     }
 });
 
