9f0e5369b942d268b5c2dc6e28f0d828ed713393
Jilu Elizebeth <elizebeth18.jilu@gmail.com>
2026-08-27 12:39:42 +0530

    fetch avatar

--- src/index.js
+++ src/index.js
@@ -12,32 +12,6 @@ app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
-const multer = require('multer');
-
-const upload = multer({
-    dest: 'images',
-    limits: {
-        fileSize: 1000000,
-
-    },
-    fileFilter: (req, file, cb) => {
-
-        if (!file.originalname.match(/\.(doc|docx)$/)) {
-            return cb(new Error('Please upload a Word Document'));
-            //return cb(new Error('File must be a PDF'))
-        }
-
-        cb(undefined, true);
-    }
-});
-
-app.post('/upload', upload.single('image'), (req, res) => {
-    res.send();
-}, (error, req, res, next) => {
-    res.status(400).send({ error: error.message })
-});
-
-
 //
 // Without midddleware: new request -> run route handler
 //
